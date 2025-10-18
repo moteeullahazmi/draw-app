73f6ea0a611c81646c9792cdb54fdbd55303e334
moteeullahazmi <moteeullahazmi@gmail.com>
2025-10-19 04:13:52 +0530

    writing structure http and ws

---
+++ apps/ws-backend/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+wss.on('connection', function connection(ws) {
+    wss.on('message', function message(data) {
+        ws.send('pong');
+    });
+});
