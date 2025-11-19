318b6eece13eb54a35e6bc55ae566c333858a8f3
moteeullahazmi <moteeullahazmi@gmail.com>
2025-11-19 11:38:50 +0530

    adding db and not run db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,27 +1,43 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "@repo/backend-common/config2"; // config name comes from pakckges/common-backend/packages.json inside export files name
+import { JWT_SECRET } from "@repo/backend-common/config"; // config name comes from pakckges/common-backend/packages.json inside export files name
 import { middleware } from "./middleware";
 import {
     CreateUserSchema,
     SignSchema,
     CreateRoomSchema,
 } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json())
 
-app.post("signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+app.post("signup", async (req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         res.json({
             message: "Incorrect inputs",
         });
         return;
     }
 
+    try {
+        await prismaClient.user.create({
+        data: {
+            email: parsedData.data?.username,
+            password: parsedData.data.password,
+            name:parsedData.data.name
+        }
+    })
     res.json({
         userId: 123,
     });
+    }
+    catch(e){
+        res.status(411).json({
+            message: "User already exist this username"
+        })
+    }
 });
 
 app.post("signin", (req, res) => {
